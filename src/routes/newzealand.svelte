bf59c293a9e408486c5e041b0dd63976809af9b4
robin <robjnz@outlook.com>
2021-12-05 13:38:08 +0000

    update

--- src/routes/newzealand.svelte
+++ src/routes/newzealand.svelte
@@ -5,8 +5,6 @@
 	import Companies from '/src/component/australasia/newzealand/Companies.svelte';
 	import Gazette from '/src/component/australasia/newzealand/Gazette.svelte';
 	import Historical from '/src/component/australasia/newzealand/Historical.svelte';
-	import Nzstate from '/src/component/australasia/newzealand/Nzstate.svelte';
-
 </script>
 
 <svelte:head>
@@ -15,7 +13,6 @@
 
 <section class="one">
 	<h1 class="ml-8 mt-3 inline-block text-center text-purple-500 text-font-sans text-4xl">
-		
 		New Zealand Family History Resourses
 	</h1>
 	<br />
@@ -23,13 +20,12 @@
 	<div class="grid sm:flex gap-4 mt-20 one">
 		<Auckland />
 		<Companies />
-        <Gazette />
+		<Gazette />
 	</div>
 
-    <div class="grid sm:flex gap-4 mt-8 one">
-    <Historical />
-    <Nzstate />
-    </div>
+	<div class="grid sm:flex gap-4 mt-8 one">
+		<Historical />
+	</div>
 </section>
 
 <style>
